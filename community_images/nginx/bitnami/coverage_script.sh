39bd3a288e025e9d5a6379c9f069d3051bcb231e
Surendra Babu <surendra.babu@apexon.com>
2024-08-27 15:38:42 +0530

    updated

--- community_images/nginx/bitnami/coverage_script.sh
+++ community_images/nginx/bitnami/coverage_script.sh
@@ -1,7 +1,26 @@
+# #!/bin/bash
+
+# set -x
+# set -e
+
+# declare -a MODULE_ARRAY=("ngx_http_brotli_static_module" "ngx_stream_geoip2_module" "ngx_http_brotli_filter_module" "ngx_http_geoip2_module");
+# for module in "${MODULE_ARRAY[@]}"
+# do
+#     echo "load_module modules/${module}.so;" | cat - /opt/bitnami/nginx/conf/nginx.conf > /tmp/nginx.conf && \
+#         cp /tmp/nginx.conf /opt/bitnami/nginx/conf/nginx.conf
+# done
+
+# /opt/bitnami/scripts/nginx/reload.sh
+
+# /opt/bitnami/scripts/nginx/status.sh
+
 #!/bin/bash
 
 set -x
 set -e
+
+# Update the Nginx user to a valid one
+sed -i 's/user www;/user nginx;/' /opt/bitnami/nginx/conf/nginx.conf
 
 declare -a MODULE_ARRAY=("ngx_http_brotli_static_module" "ngx_stream_geoip2_module" "ngx_http_brotli_filter_module" "ngx_http_geoip2_module");
 for module in "${MODULE_ARRAY[@]}"
@@ -11,6 +30,4 @@ do
 done
 
 /opt/bitnami/scripts/nginx/reload.sh
-
 /opt/bitnami/scripts/nginx/status.sh
-
